a5503046db786053cfd8a0d7c629fea2caeb7837
Marcell Kiss <marcell@kmarcell.com>
2025-11-08 15:02:18 +0100

    Add RP2040 support to remote transmitter

--- esphome/components/remote_transmitter/__init__.py
+++ esphome/components/remote_transmitter/__init__.py
@@ -151,6 +151,9 @@ FILTER_SOURCE_FILES = filter_source_files_from_platform(
             PlatformFramework.ESP32_ARDUINO,
             PlatformFramework.ESP32_IDF,
         },
+        "remote_transmitter_rp2040.cpp": {
+            PlatformFramework.RP2040_ARDUINO,
+        },
         "remote_transmitter.cpp": {
             PlatformFramework.ESP8266_ARDUINO,
             PlatformFramework.BK72XX_ARDUINO,
